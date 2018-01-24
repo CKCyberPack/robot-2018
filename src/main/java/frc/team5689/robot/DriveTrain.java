bf3113123ce485f4576f69b2541d951db9ca9cb2
Austin <austin.formagin117@gmail.com>
2018-01-24 10:06:03 -0500

    Added the RobotMap class - "Magic Numbers", added DriveTrain class with meccanum drive, both cartesian and polar. In main Robot class, added 3 different layouts for driving meccanum.

---
+++ src/main/java/frc/team5689/robot/DriveTrain.java
@@ -0,0 +1,32 @@
+package frc.team5689.robot;
+
+
+import edu.wpi.first.wpilibj.RobotDrive;
+import edu.wpi.first.wpilibj.VictorSP;
+import edu.wpi.first.wpilibj.drive.MecanumDrive;
+
+public class DriveTrain {
+    VictorSP rightFrontMotor;
+    VictorSP leftFrontMotor;
+    VictorSP rightBackMotor;
+    VictorSP leftBackMotor;
+    MecanumDrive ckDrive;
+
+
+    public DriveTrain(){
+        rightFrontMotor = new VictorSP(RobotMap.pwmRightFrontDrive);
+        leftFrontMotor = new VictorSP(RobotMap.pwmLeftFrontDrive);
+        rightBackMotor = new VictorSP(RobotMap.pwmRightBackDrive);
+        leftBackMotor = new VictorSP(RobotMap.pwmLeftBackDrive);
+        ckDrive = new MecanumDrive(leftFrontMotor, leftBackMotor, rightFrontMotor, rightBackMotor);
+        ckDrive.setSafetyEnabled(false);
+
+    }
+
+    public void teleDriveCartesian(double forward, double strafe, double rotation){
+        ckDrive.driveCartesian(forward, strafe, rotation);
+    }
+    public void teleDrivePolar(double mag, double angle, double zRot){
+        ckDrive.drivePolar(mag, angle, zRot);
+    }
+}
---
+++ src/main/java/frc/team5689/robot/RobotMap.java
@@ -0,0 +1,16 @@
+package frc.team5689.robot;
+
+/////////////////////////////////////////////////////////
+//** Magic Numbers Class ** I think
+/////////////////////////////////////////////////////////
+public class RobotMap {
+
+    //PWM outputs
+    public static int pwmLeftFrontDrive = 0;
+    public static int pwmRightFrontDrive = 1;
+    public static int pwmLeftBackDrive = 2;
+    public static int pwmRightBackDrive = 3;
+
+
+    //Robot Dependent Variables
+}
