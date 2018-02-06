443ecfaa9374b0894467c2d5a3869457509e390a
Austin <austin.formagin117@gmail.com>
2018-02-05 19:05:05 -0500

    Changed the input values for drive to be squared, readded the sucker class and added variables to robotmap

--- src/main/java/frc/team5689/ck2018/RobotMap.java
+++ src/main/java/frc/team5689/ck2018/RobotMap.java
@@ -5,30 +5,42 @@ package frc.team5689.ck2018;
 /////////////////////////////////////////////////////////
 public class RobotMap {
 
-    //PWM outputs
+    //CAN outputs
+    //-------------//
+    //Drive
     public static int canRearLeft = 11;
     public static int canRearRight = 12;
     public static int canFrontLeft = 15;
     public static int canFrontRight = 16;
-
-
-
+    //Shooters
     public static int shooterLeft = 21;
     public static int shooterRight = 22;
     public static int preshootLeft = 25;
     public static int preshootRight = 26;
+    //Intake
+    public static int suckerLeft = 0;
+    public static int suckerRight = 0;
 
+
+    //PCM
+    //-------------//
+    //Shooter
     public static int pcmShooterHigh = 0;
     public static int pcmShooterLow = 1;
     public static int pcmShooterFire = 2;
+    //Intake
+    public static int pcmSuckerLeft = 0;
+    public static int pcmSuckerRight =1;
 
 
     //Robot Dependent Variables
+    //----------------------------//
     public static double driveDeadzone = 0.1;
 
 
 
     //SmartDashboard
+    //-----------------//
     public static String robotMode = "Robot Mode";
     public static String driveMode = "Drive Mode";
     public static String gameData = "Game Data";
---
+++ src/main/java/frc/team5689/ck2018/Sucker.java
@@ -0,0 +1,39 @@
+package frc.team5689.ck2018;
+import com.ctre.phoenix.motorcontrol.can.VictorSPX;
+import com.ctre.phoenix.motorcontrol.ControlMode;
+import com.ctre.phoenix.motorcontrol.can.VictorSPX;
+import edu.wpi.first.wpilibj.Solenoid;
+import frc.team5689.ck2018.RobotMap;
+
+public class Sucker {
+    private VictorSPX suckMotorLeft;
+    private VictorSPX suckMotorRight;
+    private Solenoid leftPiston;
+    private Solenoid rightPiston;
+
+    public void Sucker(){
+        //Initialize motors and pistons
+        suckMotorLeft = new VictorSPX(RobotMap.suckerLeft);
+        suckMotorRight = new VictorSPX(RobotMap.suckerRight);
+        leftPiston = new Solenoid(RobotMap.pcmSuckerLeft);
+        rightPiston = new Solenoid(RobotMap.pcmSuckerRight);
+    }
+
+    public void open(){
+        //Opens the pistons
+        leftPiston.set(true);
+        rightPiston.set(true);
+    }
+
+    public void close(){
+        //Closes the pistons
+        leftPiston.set(false);
+        rightPiston.set(false);
+    }
+
+    public void suck (double speed){
+        //Sets the motors' speeds
+        suckMotorLeft.set(ControlMode.PercentOutput, 0.5);
+        suckMotorRight.set(ControlMode.PercentOutput, 0.5);
+    }
+}
