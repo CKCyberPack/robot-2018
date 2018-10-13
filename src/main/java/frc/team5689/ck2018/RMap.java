baedfa53e08e09c45fd1e46c6a722b1aee2cf232
Austin <austin.formagin117@gmail.com>
2018-10-13 14:56:21 -0400

    Updated RMap

--- src/main/java/frc/team5689/ck2018/RMap.java
+++ src/main/java/frc/team5689/ck2018/RMap.java
@@ -41,6 +41,7 @@ public class RMap {
     public static final int pcmShooterLow = 5;
     public static final int pcmShooterLowB = 0;
     public static final int pcmShooterFireA = 3;
+
     public static final int pcmShooterFireB = 2;
 
 
@@ -58,8 +59,9 @@ public class RMap {
     public static final double armPOW = 0.4;
     public static final double armKP = 10;//SmartDashboard.getNumber("kP", 0);
     public static final double armKI = 0;
-    public static final double armKD = 0.01;
+    public static final double armKD = 0.01;//TODO adjust these values to stop arm wiggling.
     public static final double armKF = 0;
+
     //Shooter Motors
     public static final double shootKP = 0.01;//SmartDashboard.getNumber("kP", 0);
     public static final double shootKI = 0.001;
@@ -74,11 +76,14 @@ public class RMap {
     public static final double gyroStraightKP = -0.05;  //0.065
     public static final float maxCollisionG = 0.75f;
     public static final double autoStraightSpeed = 0.5;
+    public static final double gyroTurnSpeed = 0.2;
     //Intake angles
     public static final int intakeAngle = 2000;
-    public static final int intakeAngleStop = 2500;
+    public static final int intakeAngleStop = 2700;
+    public static final int getIntakeAngleStopFling = 300;
     public static final int intakeAngleMax = 3000;
     public static final int intakeAngleSpeed = 30;
+    public static final int intakeReset = 200;
     //Intake speeds
     public static final double intakeSpeedMax = 0.5;
     //Shooter speeds
@@ -88,10 +93,11 @@ public class RMap {
     public static final double shootSpeedHigh = 1;
     public static final double shootSpeedLow = 0.3;
     public static final double shootSpeedFlat = 0.3;
+    public static final double chrisShootSpeed = 0.6;
     //Command Timing
     public static final long timerShoot = 1000;
     public static final long timerAim = 1000;
-    public static final long timerPreShoot = 2000;
+    public static final long timerPreShoot = 500;
 
 
     //SmartDashboard
@@ -100,4 +106,6 @@ public class RMap {
     public static final String driveMode = "Drive Mode";
     public static final String gameData = "Game Data";
     public static final String shootPosition = "Shooter";
+
+
 }
